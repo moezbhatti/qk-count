e422811a2c4eca1c145f1c7983ee3d090dfe6152
Moez Bhatti <moez.bhatti@gmail.com>
2015-03-16 04:20:42 -0400

    Initial commit

---
+++ app/src/main/java/com/moezbhatti/counter/MainActivity.java
@@ -0,0 +1,64 @@
+package com.moezbhatti.counter;
+
+import android.os.Vibrator;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private View mRootView;
+    private TextView mCounter;
+    private int mCount = 0;
+    private Vibrator mVibrator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRootView = findViewById(R.id.root_view);
+        mRootView.setOnClickListener(this);
+
+        mCounter = (TextView) findViewById(R.id.counter);
+        mCounter.setText(Integer.toString(mCount));
+
+        mVibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.root_view) {
+            mCount++;
+            mCounter.setText(Integer.toString(mCount));
+            mVibrator.vibrate(50);
+        }
+    }
+}

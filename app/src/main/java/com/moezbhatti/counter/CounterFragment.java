32e2049b43814f6bebb1c5ae30ad4842e06f199e
Moez Bhatti <moez.bhatti@gmail.com>
2015-03-30 00:42:53 -0400

    Added support for direction switching

--- app/src/main/java/com/moezbhatti/counter/CounterFragment.java
+++ app/src/main/java/com/moezbhatti/counter/CounterFragment.java
@@ -6,15 +6,21 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.*;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CounterFragment extends Fragment implements View.OnClickListener {
 
     private Activity mActivity;
+    private int mCount = 0;
+    private int mIncrement = 1;
+    private Vibrator mVibrator;
+
     private View mRootView;
     private TextView mCounter;
-    private int mCount = 0;
-    private Vibrator mVibrator;
+    private ImageButton mDirectionUp;
+    private ImageButton mDirectionDown;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,12 @@ public class CounterFragment extends Fragment implements View.OnClickListener {
         mCounter = (TextView) view.findViewById(R.id.counter);
         mCounter.setText(Integer.toString(mCount));
 
+        mDirectionUp = (ImageButton) view.findViewById(R.id.direction_up);
+        mDirectionUp.setOnClickListener(this);
+
+        mDirectionDown = (ImageButton) view.findViewById(R.id.direction_down);
+        mDirectionDown.setOnClickListener(this);
+
         mVibrator = (Vibrator) mActivity.getSystemService(Context.VIBRATOR_SERVICE);
 
         return view;
@@ -46,10 +58,29 @@ public class CounterFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.root_view) {
-            mCount++;
-            mCounter.setText(Integer.toString(mCount));
-            mVibrator.vibrate(50);
+        switch (v.getId()) {
+
+            case R.id.root_view:
+                if (mCount + mIncrement >= 0) {
+                    mCount += mIncrement;
+                    mCounter.setText(Integer.toString(mCount));
+                } else {
+                    Toast.makeText(mActivity, R.string.number_bottom_warning, Toast.LENGTH_SHORT).show();
+                }
+                mVibrator.vibrate(50);
+                break;
+
+            case R.id.direction_up:
+                mIncrement = 1;
+                mDirectionUp.setImageResource(R.drawable.ic_arrow_circle);
+                mDirectionDown.setImageResource(R.drawable.ic_arrow);
+                break;
+
+            case R.id.direction_down:
+                mIncrement = -1;
+                mDirectionUp.setImageResource(R.drawable.ic_arrow);
+                mDirectionDown.setImageResource(R.drawable.ic_arrow_circle);
+                break;
         }
     }
 }

e801c6e0c117913e8b983fe5c48f7d46f5b6905f
Moez Bhatti <moez.bhatti@gmail.com>
2015-04-05 22:58:26 -0400

    Circular reveal

--- app/src/main/java/com/qklabs/counter/fragment/PreferenceFragment.java
+++ app/src/main/java/com/qklabs/counter/fragment/PreferenceFragment.java
@@ -1,8 +1,9 @@
 package com.qklabs.counter.fragment;
 
+import android.animation.Animator;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.*;
+import android.view.animation.DecelerateInterpolator;
 import com.qklabs.counter.R;
 
 /**
@@ -32,5 +33,30 @@ public class PreferenceFragment extends android.preference.PreferenceFragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.findItem(R.id.action_settings).setVisible(false);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+
+        // Circular reveal for the fragment
+        if (rootView != null) {
+            rootView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+                @Override
+                public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                    v.removeOnLayoutChangeListener(this);
+
+                    int radius = (int) Math.hypot(right, bottom);
+
+                    Animator reveal = ViewAnimationUtils.createCircularReveal(v, right, top, 0, radius);
+                    reveal.setInterpolator(new DecelerateInterpolator(2f));
+                    reveal.setDuration(1000);
+                    reveal.start();
+                }
+            });
+        }
+
+        return rootView;
     }
 }
